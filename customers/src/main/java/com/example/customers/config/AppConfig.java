080390de53748f6750b2f6f02ea5b63550d1c6f0
Tatsiana <tatsiana.ramanchuk@gmail.com>
2024-04-10 16:24:10 +0300

    Error localization

---
+++ customers/src/main/java/com/example/customers/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.example.customers.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(3600);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.ENGLISH);
+        return messageSource;
+    }
+}
