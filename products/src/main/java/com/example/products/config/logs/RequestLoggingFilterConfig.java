f66e476ce179a943386110ddb1139198caf614bc
Tatsiana <tatsiana.ramanchuk@gmail.com>
2024-05-17 13:19:29 +0300

    Add requests logging in Product-service

---
+++ products/src/main/java/com/example/products/config/logs/RequestLoggingFilterConfig.java
@@ -0,0 +1,22 @@
+package com.example.products.config.logs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("Response:");
+        filter.setBeforeMessagePrefix("Request:");
+        filter.setHeaderPredicate(header -> !header.equalsIgnoreCase("authorization"));
+        return filter;
+    }
+}
